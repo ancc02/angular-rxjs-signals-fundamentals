10bc3d0f49ec649f544f20186ab0061558c3d93e
ancc02 <oandresccc@gmail.com>
2024-03-30 10:33:04 -0500

    creacion servicio producto

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +8,14 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   // Just enough here for the code to compile
   private productsUrl = 'api/products';
+  
+  private http = inject(HttpClient);
 
+  getProducts(): Observable<Prooduct[]>{
+    return 
+    this.http.get<Product[]>(this.productsUrl)
+    .pipe(
+      tap(() => console.log('In http.get pipeline'))
+    );
+  }
 }
