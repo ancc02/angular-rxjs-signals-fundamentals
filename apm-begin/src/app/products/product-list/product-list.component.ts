8f888b549779c34e9805b8ddba2403d3a7b49f0b
ancc02 <oandresccc@gmail.com>
2024-04-24 14:47:04 -0500

    declarative programming

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,45 +1,34 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { NgIf, NgFor, NgClass } from '@angular/common';
-import { Product } from '../product';
+import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { EMPTY, Subscription, catchError, tap } from 'rxjs';
+import { EMPTY, catchError, tap } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
+  imports: [AsyncPipe, NgIf, NgFor, NgClass, ProductDetailComponent],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent {
   private productService = inject(ProductService);
   pageTitle = 'Products';
   errorMessage = '';
-  sub!: Subscription;
 
   // Products
-  products: Product[] = [];
+  readonly products$ = this.productService
+    .products$
+    .pipe(
+      tap(() => console.log('In component pipeline')),
+      catchError((err) => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
-
-  ngOnInit(): void {
-    this.sub = this.productService
-      .getProducts()
-      .pipe(
-        tap(() => console.log('In component pipeline')),
-        catchError((err) => {
-          this.errorMessage = err;
-          return EMPTY;
-        })
-      )
-      .subscribe((products) => (this.products = products));
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -18,12 +18,10 @@ export class ProductService {
   private errorService = inject(HttpErrorService);
   private reviewService = inject(ReviewService);
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl).pipe(
-      tap(() => console.log('In http.get pipeline')),
-      catchError((err) => this.handleError(err))
-    );
-  }
+  readonly products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    tap(() => console.log('In http.get pipeline')),
+    catchError((err) => this.handleError(err))
+  );
 
   getProduct(id: number): Observable<Product> {
     const productUrl = this.productsUrl + '/' + id;
@@ -41,7 +39,7 @@ export class ProductService {
           map(reviews => ({ ...product, reviews } as Product))
         );
     } else {
-        return of(product);
+      return of(product);
     }
   }
 
